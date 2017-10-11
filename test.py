946543c8682aa0a9af8a8e80b0d049959343ef81
joham97 <joham97@web.de>
2017-10-11 14:41:19 +0200

    Result Method

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-from bottle import route, run
+from bottle import route, run, response, get, post, request
+from json import dumps
 import socket
 
-@route('/teamspeak')
+@get('/teamspeak')
 def teamspeakOnline():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	result = sock.connect_ex(('testoknof.de', 30033))
-	if result == 0:
-		return "teamspeak is online"
-	else:	
-		return "teamspeak is offline"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(('testoknof.de', 30033))
+    if result == 0:
+        return sendResult(0)
+    else:	
+        return sendResult(1)
 
+def sendResult(value):
+    rv = { "result": value }
+    response.content_type = 'application/json'
+    return dumps(rv)
+
+#run(host='10.112.16.123', port=8080, debug=True)
 run(host='testoknof.de', port=8080, debug=True)
